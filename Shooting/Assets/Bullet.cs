35295903d96cd92f332c24ac09c155d364aae9d7
LAPTOP-TAFL9MMM\syuu1 <s.yuu12.nawate@gmail.com>
2017-02-28 23:15:48 +0900

    シューティングゲームの練習開始

---
+++ Shooting/Assets/Bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+	private float bulletlifetime = 0.5f;
+
+	void Update()
+	{
+		bulletlifetime -= Time.deltaTime;
+		if (bulletlifetime <= 0) {
+			Destroy (this.gameObject);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		GameObject other = collision.gameObject;
+		if(other.layer== 8){
+			Destroy(other.gameObject);
+		}
+	}
+
+}
---
+++ Shooting/Assets/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour 
+{
+	public Rigidbody BarrelPrefub;
+	public Transform BarrelTransForm;
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.S)){
+			Rigidbody rocketbarrel;
+			rocketbarrel = Instantiate (BarrelPrefub, BarrelTransForm.position, BarrelTransForm.rotation)as Rigidbody;
+			rocketbarrel.AddForce (BarrelTransForm.right * 500);
+		}
+	}
+}
