8ba0b72a51696c55739d0d2ae60a0734a699f00d
LAPTOP-TAFL9MMM\syuu1 <s.yuu12.nawate@gmail.com>
2017-03-01 20:09:49 +0900

    クリックした位置に正面を向ける

---
+++ Shooting/Assets/MovePlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour 
+{
+	private Vector2 destination;
+	private float movespeed = 3.0f;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			destination = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		}
+
+		transform.position = Vector2.MoveTowards (transform.position, new Vector2 (destination.x, destination.y), movespeed * Time.deltaTime);
+	}
+}
